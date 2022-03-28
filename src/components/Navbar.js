9a6c6185b3c26c6c6384f1a26ca4fd915acf3216
justfawzi <81636598+justfawzi@users.noreply.github.com>
2022-03-28 23:29:53 +0100

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+            <a class="navbar-brand" href="#">Randa Boumghar</a>
+            <button style={{ backgroundColor: 'red !important' }} class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span style={{ backgroundColor: 'red !important' }} class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Creations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/Work.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Work() {
+    return (
+        <div className='work pt-5'>
+            <div className='container'>
+                <h1>Creations</h1>
+                <div className='row mt-5'>
+                    <div className='col mb-5'>
+                        <div className='box brodery'>
+                            <div className='overlay'></div>
+                            <h1>Brodery</h1>
+                        </div>
+                    </div>
+                    <div className='col mt-2'>
+                        <div className='box music'>
+                            <div className='overlay'></div>
+                            <h1>Music</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
